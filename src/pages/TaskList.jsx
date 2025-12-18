40e6fe0e1d6e6181996a1343127cf8497980005d
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-12-18 12:06:40 +0100

    milestone 3

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,29 +1,36 @@
 import { useContext } from 'react'
 import { GlobalContext } from '../contexts/GlobalContext'
+import TaskRow from '../components/TaskRow'
 
 const TaskList = () => {
 
-    const {tasks} = useContext(GlobalContext)
+    const { tasks } = useContext(GlobalContext)
     console.log("tasks: ", tasks)
-  return (
-    <div className="container">
-        <div className="row d-flex">
+    return (
+        <div className="container">
+            <div className="row d-flex">
+                <div className="col-12">
+                    <h1>Lista Tasks</h1>
+                </div>
+            </div>
             <div className="col-12">
-                <h1>Lista Tasks</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Stato</th>
+                            <th>Data di creazione</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tasks.map(task => (
+                            <TaskRow key={task.id} task={task} />
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
-        <div className="col-12">
-            {tasks.map((task)=>{
-                return (
-                    <>
-                        <p><strong>TASK:</strong> {task.title}</p>
-                        <p><strong>COSA FARE:</strong> {task.description}</p>
-                    </>
-                )
-            })}
-        </div>
-    </div>
-  )
+    )
 }
 
 export default TaskList
